1599dea86ac6439003d4e50c2f71462d4d8f7637
xjc <2585441871@qq.com>
2018-11-06 15:51:47 +0800

    菜单

--- app/controller/authMenu.js
+++ app/controller/authMenu.js
@@ -4,14 +4,18 @@ const Controller = require('egg').Controller;
 const moment = require('moment');
 
 function fn(data, pid) {
-  var result = [], temp;
-  for (var i = 0; i < data.length; i++) {
+  let result = [];
+  let temp = [];
+  for (let i = 0; i < data.length; i++) {
     if (data[i].pid == pid) {
-      var obj = {
-        name: data[i].cate_name,
+      let obj = {
+        name: data[i].name,
         id: data[i].id
       };
       temp = fn(data, data[i].id);
+
+      console.log('temp', temp)
+
       if (temp.length > 0) {
         obj.children = temp;
       }
@@ -24,20 +28,17 @@ function fn(data, pid) {
 
 class TestController extends Controller {
   /**
-   * 需求：权限菜单
+   * 需求：权限菜单-列表
    * 版本号：v1.0.0
    */
   async list() {
     const { ctx } = this;
-
     const list = await this.app.mysql.select('deep_cate', {
-      // where: {
-      //   pid: 0,
-      // },
+      where: {
+        status: 1, // 是否可用
+      },
       columns: [ 'id', 'pid', 'cate_name' ],
     });
-
-
     let fnList = fn(list, 0)
 
     ctx.body = {
@@ -49,6 +50,135 @@ class TestController extends Controller {
       },
     };
   }
+  /**
+   * 需求：权限菜单-新增
+   * 1，判断名字是否存在。
+   * 2，判断pid是否存在。
+   * 3，新增
+   */
+  async create() {
+    const { ctx } = this;
+
+    if (!ctx.request.body.name) {
+      ctx.body = {
+        status: 501,
+        message: '菜单名称不存在',
+      };
+      return false;
+    }
+    if (!ctx.request.body.pid) {
+      ctx.body = {
+        status: 501,
+        message: 'pid不存在',
+      };
+      return false;
+    }
+
+    const list = await this.app.mysql.get('deep_cate', {
+      status: 1, // 是否可用
+      name: ctx.request.body.name,
+    });
+    if (list) {
+      ctx.body = {
+        status: 501,
+        message: '菜单名称已经存在，请重新选择菜单名称',
+      };
+    } else {
+      const result = await this.app.mysql.insert('deep_cate', {
+        name: ctx.request.body.name,
+        pid: ctx.request.body.pid,
+      });
+      if (result) {
+        ctx.body = {
+          status: 200,
+          message: '新增成功',
+        };
+      } else {
+        ctx.body = {
+          status: 501,
+          message: '数据库错误',
+        };
+      }
+
+    }
+  }
+  /**
+   * 需求：权限菜单-编辑菜单
+   * 版本号：v1.0.0
+   */
+  async update() {
+    const { ctx } = this;
+
+    if (!ctx.request.body.id) {
+      ctx.body = {
+        status: 501,
+        message: 'id不存在',
+      };
+      return false;
+    }
+
+    if (!ctx.request.body.name) {
+      ctx.body = {
+        status: 501,
+        message: '菜单名称不存在',
+      };
+      return false;
+    }
+
+    const list = await this.app.mysql.update('deep_cate', {
+      name: ctx.request.body.name,
+      id: ctx.request.body.id,
+    });
+
+    if (list.affectedRows === 1) {
+      ctx.body = {
+        status: 200,
+        message: '更新成功',
+      };
+    } else {
+      ctx.body = {
+        status: 501,
+        message: '数据库错误',
+      };
+    }
+  }
+
+  /**
+   * 需求：权限菜单-删除菜单
+   * 版本号：v1.0.0
+   * 逻辑删除
+   * 1，判断id是不是存在，存在让status变成0
+   */
+  async delete() {
+    const { ctx } = this;
+
+    if (!ctx.request.body.id) {
+      ctx.body = {
+        status: 501,
+        message: 'id不存在',
+      };
+      return false;
+    }
+    const list = await this.app.mysql.update('deep_cate', {
+      status: 0, // 是否可用
+      id: ctx.request.body.id,
+    });
+
+    if (list.affectedRows === 1) {
+      ctx.body = {
+        status: 200,
+        message: '删除成功',
+      };
+    } else {
+      ctx.body = {
+        status: 501,
+        message: '数据库错误',
+      };
+    }
+
+
+  }
+
 }
 
 module.exports = TestController;
--- app/router.js
+++ app/router.js
@@ -12,5 +12,9 @@ module.exports = app => {
   apiRouter.post('/isSignIn', controller.login.isSignIn); // 判断是都登录
   apiRouter.post('/getUserId', controller.login.getUserId); // 获取用户id
   apiRouter.post('/signOut', controller.login.signOut); // 登出
-  apiRouter.post('/authMenuList', controller.authMenu.list); // 权限菜单-列表
+
+  apiRouter.post('/authMenu/list', controller.authMenu.list); // 权限菜单-列表
+  apiRouter.post('/authMenu/create', controller.authMenu.create); // 权限菜单-新增
+  apiRouter.post('/authMenu/delete', controller.authMenu.delete); // 权限菜单-删除
+  apiRouter.post('/authMenu/update', controller.authMenu.update); // 权限菜单-更新
 };
