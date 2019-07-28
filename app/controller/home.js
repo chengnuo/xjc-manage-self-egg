cbac4c938c58a9586e30a5b59aed4048afc7efe5
xjc <2585441871@qq.com>
2019-07-28 11:19:05 +0800

    整理mp3

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,10 +2,22 @@
 
 const Controller = require('egg').Controller;
 
+const fs = require('fs');
+
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'hi, egg';
+    const  newConceptOne = fs.readdirSync('./app/public/mp3/newConceptOne');
+    const  newConceptTwo = fs.readdirSync('./app/public/mp3/newConceptTwo');
+    const  newConceptThree = fs.readdirSync('./app/public/mp3/newConceptThree');
+    const  newConceptFour = fs.readdirSync('./app/public/mp3/newConceptFour');
+    ctx.body = {
+      name: 'hi, egg',
+      newConceptOne: newConceptOne,
+      newConceptTwo: newConceptTwo,
+      newConceptThree: newConceptThree,
+      newConceptFour: newConceptFour,
+    };
   }
 }
 
